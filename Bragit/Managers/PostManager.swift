e14ea463c8ca1bd6bb17ec60f74a97a7dc48d5aa
juseojo <j86371224@gmail.com>
2025-08-21 14:53:01 +0900

    ✨ Feat: 메인 피드 게시글 가져오기 기능 구현

--- Bragit/Managers/PostManager.swift
+++ Bragit/Managers/PostManager.swift
@@ -2,5 +2,40 @@
 //  PostManager.swift
 //  Bragit
 //
-//  Created by 이태윤 on 8/20/25.
+//  Created by seongjun cho on 8/20/25.
 //
+
+import Foundation
+import Supabase
+
+class PostManager {
+  private let client: SupabaseClient
+
+  init() {
+    let apiKey = Bundle.main.infoDictionary?["Supabase api"] as? String ?? ""
+    let urlString = Bundle.main.infoDictionary?["Supabase URL"] as? String ?? ""
+
+    if apiKey.isEmpty || urlString.isEmpty {
+      fatalError("⚠️ SUPABASE_API_KEY, SUPABASE_URL Config 설정 빠짐!!")
+    }
+
+    if let url = URL(string: "https://" + urlString) {
+      client = SupabaseClient(supabaseURL: url, supabaseKey: apiKey)
+    } else {
+      fatalError("⚠️ URL 구성 오류")
+    }
+  }
+
+  // 메인 피드 게시글 가져오기
+  func fetchMainFeedData() async throws -> [Post] {
+    var post = [Post]()
+
+    post = try await client
+      .from("Post")
+      .select("*, Tag(*), comment_count:Comment(count)")
+      .execute()
+      .value
+
+    return post
+  }
+}
