399e1f55f3cda566c4cae4065af9d0079fee1b12
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-08-26 12:55:19 +0900

    ✨ Feat: 관심 리액터 생성후 관심VC 리액터 연결

--- Bragit/Presentation/Favorite/FavoriteViewController.swift
+++ Bragit/Presentation/Favorite/FavoriteViewController.swift
@@ -6,8 +6,24 @@
 //
 import UIKit
 
-class FavoriteViewController: UIViewController {
+import ReactorKit
+import RxSwift
+import RxCocoa
 
+class FavoriteViewController: UIViewController {
+  private let reactor: FavoriteReactor
+  private let disposeBag = DisposeBag()
+
+  init(reactor: FavoriteReactor) {
+    self.reactor = reactor
+    super.init(nibName: nil, bundle: nil)
+    modalPresentationStyle = .fullScreen
+    modalTransitionStyle = .crossDissolve
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .blue
