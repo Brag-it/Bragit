14b58fc8939190d4ad0ecebcbb96c3ae5f8f7780
juseojo <j86371224@gmail.com>
2025-08-26 20:03:57 +0900

    ✨ Feat: 상단 페이지 타입(태그, 사용자) 선택 뷰와 기능 구현

---
+++ Bragit/Presentation/Favorite/FavoriteView.swift
@@ -0,0 +1,61 @@
+//
+//  FavoriteView.swift
+//  Bragit
+//
+//  Created by seongjun cho on 8/26/25.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class FavoriteView: UIView {
+  private let headerView = UIView().then {
+    $0.backgroundColor = .white
+  }
+
+  private let searchButton = UIButton().then {
+    $0.setImage(.search, for: .normal)
+  }
+
+  let choiceButton = UIButton().then {
+    $0.setImage(.drop, for: .normal)
+    $0.setTitle("태그", for: .normal)
+    $0.setTitleColor(UIColor(red: 0.34, green: 0.34, blue: 0.34, alpha: 1), for: .normal)
+    $0.titleLabel?.font = .pretendard(size: 18, weight: .semibold)
+    $0.semanticContentAttribute = .forceRightToLeft
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    setUI()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setUI() {
+    addSubview(headerView)
+    headerView.addSubview(searchButton)
+    headerView.addSubview(choiceButton)
+
+    headerView.snp.makeConstraints {
+      $0.top.leading.trailing.equalTo(safeAreaLayoutGuide)
+      $0.height.equalTo(58)
+    }
+
+    searchButton.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.width.height.equalTo(24)
+      $0.trailing.equalToSuperview().inset(20)
+    }
+
+    choiceButton.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.leading.equalToSuperview().offset(20)
+    }
+  }
+}
