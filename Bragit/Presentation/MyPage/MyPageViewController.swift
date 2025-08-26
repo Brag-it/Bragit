24bd1eb0523e64377ac6580f9a19d2d1c8ed4960
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-08-26 12:53:13 +0900

    ✨ Feat: 마이페이지 리액터 VC와 연결

--- Bragit/Presentation/MyPage/MyPageViewController.swift
+++ Bragit/Presentation/MyPage/MyPageViewController.swift
@@ -6,8 +6,24 @@
 //
 import UIKit
 
-class MyPageViewController: UIViewController {
+import ReactorKit
+import RxSwift
+import RxCocoa
 
+class MyPageViewController: UIViewController {
+  private let reactor: MyPageReactor
+  private let disposeBag = DisposeBag()
+
+  init(reactor: MyPageReactor) {
+    self.reactor = reactor
+    super.init(nibName: nil, bundle: nil)
+    modalPresentationStyle = .fullScreen
+    modalTransitionStyle = .crossDissolve
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .green
