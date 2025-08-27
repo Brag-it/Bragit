8ff3aca7ded3b304bf71a6329c7e1da792c62d47
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-08-27 10:39:38 +0900

    ✨ Feat: TextKit2를 위한 필요 객체들 선언

--- Bragit/Presentation/Write/WriteViewController.swift
+++ Bragit/Presentation/Write/WriteViewController.swift
@@ -51,9 +51,17 @@ class WriteViewController: UIViewController, View {
     $0.backgroundColor = .lightGray
   }
 
-  private let textField = UITextView().then {
+  // 텍스트의 시각적 스타일과 레이아웃을 담당하는 TextKit2의 핵심 렌더링 매니저
+  private let layoutManger = NSTextLayoutManager()
+  // 텍스트 데이터를 저장하는 객체
+  private let contentStorage = NSTextStorage()
+  // 텍스트가 배치될 위치와 영역을 정의하는 컨테이너
+  private let textContainer = NSTextContainer()
+
+  private let textField = UITextView(frame: .zero, textContainer: nil).then {
     $0.font = .pretendard(size: 16)
     $0.backgroundColor = .systemBackground
+    $0.isEditable = true
     $0.isScrollEnabled = true                           // 스크롤 가능 여부
     $0.showsVerticalScrollIndicator = false             // 수직 스크롤 바
     $0.keyboardDismissMode = .onDrag                    // 드래그 시 키보드 내려가기
