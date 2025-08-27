7eeeade7bb97497cad2dea1397052965d5e7b664
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-08-27 16:11:30 +0900

    📦 Add: 게시물 초안 모델 생성

---
+++ Bragit/Model/PostDraft.swift
@@ -0,0 +1,12 @@
+//
+//  PostDraft.swift
+//  Bragit
+//
+//  Created by 이태윤 on 8/27/25.
+//
+struct PostDraft {
+  var title: String            // 제목
+  var content: String          // 내용
+  var thumbnailImage: String?  // 썸네일 (임베딩 했을 경우)
+  var description: String?     // 미리보기 글 (임베딩 했을 경우)
+}
