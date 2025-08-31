1f61b54e860ed02894f554a70ee2577259afc268
juseojo <j86371224@gmail.com>
2025-09-01 00:57:05 +0900

    ✨ Feat: 로그인시 차단유저와 팔로우 유저 받아오기 추가

--- Bragit/App/SceneDelegate.swift
+++ Bragit/App/SceneDelegate.swift
@@ -21,29 +21,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     _ scene: UIScene,
     willConnectTo session: UISceneSession,
     options connectionOptions: UIScene.ConnectionOptions) {
-      guard let windowScene = (scene as? UIWindowScene) else { return }
+    guard let windowScene = (scene as? UIWindowScene) else { return }
 
-      window = UIWindow(windowScene: windowScene)
+    window = UIWindow(windowScene: windowScene)
 
-      // 네비게이션 로그
-      coordinator.rx.didNavigate
-        .subscribe { flow, step in
-          print("didNavigate → flow: \(flow), step: \(step)")
-        }
-        .disposed(by: disposeBag)
+    // 네비게이션 로그
+    coordinator.rx.didNavigate
+      .subscribe { flow, step in
+        print("didNavigate → flow: \(flow), step: \(step)")
+      }
+      .disposed(by: disposeBag)
 
-      guard let window = window else { return }
-      let appFlow = AppFlow(window: window)
-      let appStepper = AppStepper()
-      coordinator.coordinate(flow: appFlow, with: appStepper)
+    guard let window = window else { return }
+    let appFlow = AppFlow(window: window)
+    let appStepper = AppStepper()
+    coordinator.coordinate(flow: appFlow, with: appStepper)
 
-      // TODO: 로그인한 유저 UUID 등록하기
-      // example
-      // nowUser의 set은 UserDefaults를 사용해야함
-      UserDefaults.standard.set("e2f38754-f46b-4e3d-9347-b1ce68dc57ba", forKey: LocalStorageCase.nowUser.rawValue)
+    // TODO: 로그인한 유저 UUID 등록하기
+    // example
+    // nowUser의 set은 UserDefaults를 사용해야함
+    UserDefaults.standard.set("3d56b3b4-be2c-430f-a753-0abb914c5bc1", forKey: LocalStorageCase.nowUser.rawValue)
+    @LocalStorage(location: .favoriteTags) var favoriteTags: [Tag]?
+    // 태그 테스트 코드
+    favoriteTags = [Tag(id: "6910eac9-eb3b-4ed0-9126-29f9dfa8ce40", tag: "test",count: 0),
+                    Tag(id: "0d84179f-3d0f-49a4-b80f-8a2fca52fc76", tag: "test2", count: 0)]
 
-      setBlockUsers()
-    }
+    setBlockUsers()
+    setFollowUser()
+  }
 
   func sceneDidDisconnect(_ scene: UIScene) {
   }
@@ -66,7 +71,8 @@ extension SceneDelegate {
     let blockManager = BlockManager()
     Task {
       do {
-        @LocalStorage(location: .blockUser) var user = try await blockManager.fetchMyBlockUsers()
+        @LocalStorage(location: .blockUser) var user: [String]?
+        user = try await blockManager.fetchMyBlockUsers()
       } catch {
         print(error)
       }
@@ -77,7 +83,10 @@ extension SceneDelegate {
     let userManager = UserManager()
     Task {
       do {
-        @LocalStorage(location: .followUser) var user = try await userManager.fetchFollowUsers()
+        @LocalStorage(location: .followUser) var user: [String]?
+        user = try await userManager.fetchFollowUsers()
+        print("🌷")
+        print(user)
       } catch {
         print(error)
       }
