343ac3dd4623a4d6fb096ecba23b108a0474e191
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-09-02 19:46:24 +0900

    ✨ Feat: 서치바 컴포넌트 스택뷰로 변경 및 우선순위 설정

--- Bragit/Presentation/DesignSystem/SerachBar.swift
+++ Bragit/Presentation/DesignSystem/SerachBar.swift
@@ -14,11 +14,13 @@ import RxCocoa
 final class SearchBar: UIView {
   var disposeBag = DisposeBag()
 
-  let container = UIView().then {
+  let stackView = UIStackView().then {
     $0.backgroundColor = .white
-    $0.layer.cornerRadius = 12
-    $0.layer.borderWidth = 1
-    $0.layer.borderColor = UIColor.grayScale100.cgColor
+    $0.axis = .horizontal
+    $0.spacing = 10
+    $0.alignment = .center
+    $0.isLayoutMarginsRelativeArrangement = true
+    $0.layoutMargins = UIEdgeInsets(top: 8, left: 12, bottom: 8, right: 12)
   }
 
   let textField = UITextField().then {
@@ -28,34 +30,29 @@ final class SearchBar: UIView {
     $0.clearButtonMode = .never
     $0.autocorrectionType = .no
     $0.autocapitalizationType = .none
+    $0.setContentHuggingPriority(.defaultLow, for: .horizontal)
+    $0.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
   }
 
   let searchButton = UIButton(type: .system).then {
     $0.setImage(.search, for: .normal)
     $0.tintColor = .grayScaleBack
+    $0.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    $0.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
   }
 
   override init(frame: CGRect) {
     super.init(frame: frame)
-    addSubview(container)
-    container.addSubview(textField)
-    container.addSubview(searchButton)
+    self.layer.cornerRadius = 12
+    self.layer.borderWidth = 1
+    self.layer.borderColor = UIColor.grayScale100.cgColor
+    self.addSubview(stackView)
+    stackView.addArrangedSubview(textField)
+    stackView.addArrangedSubview(searchButton)
 
-    container.snp.makeConstraints {
+    stackView.snp.makeConstraints {
       $0.directionalEdges.equalToSuperview()
       $0.height.equalTo(48)
-    }
-
-    textField.snp.makeConstraints {
-      $0.top.bottom.equalToSuperview().inset(8)
-      $0.leading.equalToSuperview().inset(12)
-      $0.trailing.equalTo(searchButton.snp.leading).offset(-10)
-    }
-
-    searchButton.snp.makeConstraints {
-      $0.trailing.equalToSuperview().inset(12)
-      $0.centerY.equalToSuperview()
-      $0.height.equalTo(textField)
     }
   }
 
