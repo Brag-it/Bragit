03fe7314b10052880524368672da75a80e7fbbd6
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-09-09 03:22:49 +0900

    🚨 Fix: 버그 해결 작성자 id와 접속된 유저의 id의 대소문자 때에 viewer가 false로 뜨는 문제였습니다.

--- Bragit/Presentation/DetailPost/DetailPostReactor.swift
+++ Bragit/Presentation/DetailPost/DetailPostReactor.swift
@@ -66,7 +66,7 @@ class DetailPostReactor: Reactor, Stepper {
       content: DetailPostReactor.unarchivedContent(content: post.content),
       nickName: post.author?.nickname ?? "탈퇴한 유저 입니다",
       profileImage: post.author?.profile ?? nil,
-      viewer: post.author?.id == nowUser,
+      viewer: post.author?.id.lowercased() == nowUser?.lowercased(),
       isLiked: likePosts?.contains { $0 == post.id.uuidString } ?? false,
       likeCount: post.like,
       isfollowed: followUser?.contains { $0 == post.author?.id } ?? false
