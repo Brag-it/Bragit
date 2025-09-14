a433adb3378306e4fbaf32418c10a256cb325cd4
Luca Park <qlife1146@gmail.com>
2025-09-15 02:00:00 +0900

    🎨: MailInfo 레이아웃

--- Bragit/Presentation/Auth/SignUp/Mail/MailInfo/MailInfoReactor.swift
+++ Bragit/Presentation/Auth/SignUp/Mail/MailInfo/MailInfoReactor.swift
@@ -5,3 +5,24 @@
 //  Created by luca on 9/14/25.
 //
 
+import Foundation
+
+import Dependencies
+import ReactorKit
+import RxSwift
+
+final class MailInfoReactor: Reactor {
+  enum Action {
+
+  }
+
+  enum Mutation { }
+
+  struct State {}
+
+  let initialState: State
+
+  init() {
+    self.initialState = State()
+  }
+}
--- Bragit/Presentation/Auth/SignUp/Mail/MailInfo/MailInfoViewController.swift
+++ Bragit/Presentation/Auth/SignUp/Mail/MailInfo/MailInfoViewController.swift
@@ -1,3 +1,4 @@
+import Then
 //
 //  MailInfoViewController.swift
 //  Bragit
@@ -5,6 +6,275 @@
 //  Created by luca on 9/14/25.
 //
 
+import UIKit
+
 // TODO: 이메일은 앞에서 끌어오기
 // TODO: 비밀번호, 비밀번호 확인, 닉네임 TextField
 // TODO: Validate, 닉네임 중복 체크
+
+final class MailInfoViewController: UIViewController {
+  let descriptionLabel = UILabel().then {
+    $0.text = "로그인에 사용할 이메일과\n비밀번호를 입력해 주세요"
+    $0.numberOfLines = 2
+    $0.lineBreakMode = .byWordWrapping
+    $0.setContentHuggingPriority(.required, for: .vertical)
+    $0.setContentCompressionResistancePriority(.required, for: .vertical)
+    $0.font = .pretendard(size: 20, weight: .semibold)
+    $0.textColor = .grayScale900
+  }
+
+  let scrollView = UIScrollView().then {
+    $0.keyboardDismissMode = .interactive
+    $0.isUserInteractionEnabled = true
+    $0.alwaysBounceVertical = true
+    $0.showsVerticalScrollIndicator = true
+    $0.delaysContentTouches = false
+  }
+
+  let contentView = UIView().then {
+    $0.backgroundColor = .clear
+    $0.isUserInteractionEnabled = true
+  }
+
+  let mailLabel = UILabel().then {
+    $0.text = "이메일"
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .grayScale700
+  }
+
+  let mailTextField = UITextField().then {
+    $0.placeholder = "bragit@bragit.com"
+    $0.layer.borderColor = UIColor.grayScale100.cgColor
+    $0.layer.borderWidth = 1
+    $0.layer.cornerRadius = 14
+    $0.isEnabled = true
+    $0.clearButtonMode = .whileEditing
+    $0.font = .pretendard(size: 14, weight: .regular)
+    $0.textColor = .grayScale900
+    $0.returnKeyType = .next
+    $0.autocapitalizationType = .none
+    $0.spellCheckingType = .no
+    $0.autocorrectionType = .no
+    $0.textContentType = .username
+  }
+
+  let mailCheckIcon = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+
+  let mailCheckLabel = UILabel().then {
+    $0.text = "인증 완료된 이메일입니다"
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .systemSafe
+  }
+
+  let mailCheckStack = UIStackView().then {
+    $0.axis = .horizontal
+    $0.spacing = 4
+    $0.alignment = .leading
+  }
+
+  let mailStack = UIStackView()
+
+  let pwLabel = UILabel().then {
+    $0.text = "비밀번호"
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .grayScale700
+  }
+
+  let pwTextField = UITextField().then {
+    $0.placeholder = "8-24자 사이 영문, 숫자, 특수문자"
+    $0.layer.borderColor = UIColor.grayScale100.cgColor
+    $0.layer.borderWidth = 1
+    $0.layer.cornerRadius = 14
+    $0.isEnabled = true
+    $0.isSecureTextEntry = true
+    $0.textContentType = .password
+    $0.clearButtonMode = .whileEditing
+    $0.font = .pretendard(size: 14, weight: .regular)
+    $0.textColor = .grayScale900
+    $0.returnKeyType = .next
+    $0.spellCheckingType = .no
+    $0.autocorrectionType = .no
+    $0.autocapitalizationType = .none
+  }
+
+  let pwCheckIcon = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+
+  let pwCheckLabel = UILabel().then {
+    $0.text = " "
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .systemSafe
+  }
+
+  let pwCheckStack = UIStackView().then {
+    $0.axis = .horizontal
+    $0.spacing = 4
+    $0.alignment = .leading
+  }
+
+  let pwStack = UIStackView()
+
+  let rePwLabel = UILabel().then {
+    $0.text = "비밀번호 확인"
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .grayScale700
+  }
+
+  let rePwTextField = UITextField().then {
+    $0.placeholder = "8-24자 사이 영문, 숫자, 특수문자"
+    $0.layer.borderColor = UIColor.grayScale100.cgColor
+    $0.layer.borderWidth = 1
+    $0.layer.cornerRadius = 14
+    $0.isEnabled = true
+    $0.isSecureTextEntry = true
+    $0.textContentType = .password
+    $0.clearButtonMode = .whileEditing
+    $0.font = .pretendard(size: 14, weight: .regular)
+    $0.textColor = .grayScale900
+    $0.returnKeyType = .next
+    $0.spellCheckingType = .no
+    $0.autocorrectionType = .no
+    $0.autocapitalizationType = .none
+  }
+
+  let rePwCheckIcon = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+
+  let rePwCheckLabel = UILabel().then {
+    $0.text = " "
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .systemSafe
+  }
+
+  let rePwCheckStack = UIStackView().then {
+    $0.axis = .horizontal
+    $0.spacing = 4
+    $0.alignment = .leading
+  }
+
+  let rePwStack = UIStackView()
+
+  let nicknameLabel = UILabel().then {
+    $0.text = "닉네임"
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .grayScale700
+  }
+
+  let nicknameTextField = UITextField().then {
+    $0.placeholder = "8-24자 사이 영문, 숫자, 특수문자"
+    $0.layer.borderColor = UIColor.grayScale100.cgColor
+    $0.layer.borderWidth = 1
+    $0.layer.cornerRadius = 14
+    $0.isEnabled = true
+    $0.isSecureTextEntry = true
+    $0.textContentType = .password
+    $0.clearButtonMode = .whileEditing
+    $0.font = .pretendard(size: 14, weight: .regular)
+    $0.textColor = .grayScale900
+    $0.returnKeyType = .next
+    $0.spellCheckingType = .no
+    $0.autocorrectionType = .no
+    $0.autocapitalizationType = .none
+  }
+
+  let nicknameCheckIcon = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+
+  let nicknameCheckLabel = UILabel().then {
+    $0.text = " "
+    $0.font = .pretendard(size: 13, weight: .medium)
+    $0.textColor = .systemSafe
+  }
+
+  let nicknameCheckStack = UIStackView().then {
+    $0.axis = .horizontal
+    $0.spacing = 4
+    $0.alignment = .leading
+  }
+
+  let nicknameStack = UIStackView()
+
+  let nextButton = UIButton(type: .system).then {
+    $0.setTitle("다음", for: .normal)
+    $0.setTitleColor(.grayScale900, for: .normal)
+    $0.titleLabel?.font = .pretendard(size: 16, weight: .medium)
+    $0.layer.cornerRadius = 12
+    $0.isEnabled = false
+    $0.backgroundColor = .primary400
+
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+
+  private func configureUI() {
+    view.addSubview(descriptionLabel)
+    view.addSubview(scrollView)
+    scrollView.addSubview(contentView)
+
+    [mailCheckIcon, mailCheckLabel].forEach { mailCheckStack.addArrangedSubview($0) }
+    [pwCheckIcon, pwCheckLabel].forEach { pwCheckStack.addArrangedSubview($0) }
+    [rePwCheckIcon, rePwCheckLabel].forEach { rePwCheckStack.addArrangedSubview($0) }
+    [nicknameCheckIcon, nicknameCheckLabel].forEach { nicknameCheckStack.addArrangedSubview($0) }
+
+    [mailLabel, mailTextField, mailCheckStack].forEach { mailStack.addArrangedSubview($0) }
+    [pwLabel, pwTextField, pwCheckStack].forEach { pwStack.addArrangedSubview($0) }
+    [rePwLabel, rePwTextField, rePwCheckStack].forEach { rePwStack.addArrangedSubview($0) }
+    [nicknameLabel, nicknameTextField, nicknameCheckStack].forEach { nicknameStack.addArrangedSubview($0) }
+
+    [mailStack, pwStack, rePwStack, nicknameStack, nextButton].forEach { contentView.addSubview($0) }
+    [mailTextField, pwTextField, rePwTextField, nicknameTextField].forEach {
+      $0.snp.makeConstraints { $0.height.equalTo(52) }
+    }
+
+    descriptionLabel.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+      $0.leading.trailing.equalToSuperview().inset(20)
+    }
+
+    scrollView.snp.makeConstraints {
+      $0.top.equalTo(descriptionLabel.snp.bottom)
+      $0.leading.trailing.equalToSuperview()
+      $0.bottom.equalTo(view.safeAreaLayoutGuide)
+    }
+
+    contentView.snp.makeConstraints {
+      $0.edges.equalTo(scrollView.contentLayoutGuide)
+      $0.width.equalTo(scrollView.frameLayoutGuide)
+      $0.height.greaterThanOrEqualTo(scrollView.frameLayoutGuide)
+    }
+
+    mailStack.snp.makeConstraints {
+      $0.top.equalTo(contentView.snp.top).offset(24)
+    }
+
+    pwStack.snp.makeConstraints {
+      $0.top.equalTo(mailStack.snp.bottom).offset(24)
+      $0.leading.trailing.equalTo(contentView).inset(20)
+    }
+
+    rePwStack.snp.makeConstraints {
+      $0.top.equalTo(pwStack.snp.bottom).offset(24)
+      $0.leading.trailing.equalTo(contentView).inset(20)
+    }
+
+    nicknameStack.snp.makeConstraints {
+      $0.top.equalTo(rePwStack.snp.bottom).offset(24)
+      $0.leading.trailing.equalTo(contentView).inset(20)
+    }
+    
+    nextButton.snp.makeConstraints {
+      $0.top.greaterThanOrEqualTo(nicknameStack.snp.bottom).offset(24)
+      $0.leading.trailing.equalTo(contentView).inset(20)
+      $0.height.equalTo(52)
+      $0.bottom.equalTo(contentView.safeAreaLayoutGuide).inset(24)
+    }
+  }
+}
