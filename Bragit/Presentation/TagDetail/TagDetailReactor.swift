42def15f6471312becb0642e59dcec097b714237
juseojo <j86371224@gmail.com>
2025-09-15 15:39:50 +0900

    ✨ Feat: 태그 상세에서 유저 프로필로 이동 추가

--- Bragit/Presentation/TagDetail/TagDetailReactor.swift
+++ Bragit/Presentation/TagDetail/TagDetailReactor.swift
@@ -27,6 +27,7 @@ class TagDetailReactor: Reactor, Stepper {
     case followDidTap(Post)
     case tagFollowDidTap
     case didTapPost(Post)
+    case userProfileTapped(Post)
   }
 
   // 상태변경 이벤트 정의 (상태를 어떻게 바꿀 것인가)
@@ -112,6 +113,17 @@ class TagDetailReactor: Reactor, Stepper {
     case .didTapPost(let post):
       self.steps.accept(AppStep.feedDetail(post: post))
       return .empty()
+    case .userProfileTapped(let post):
+      guard let authorId = post.author?.id, !authorId.isEmpty else {
+        return .empty()
+      }
+      return userManager
+        .rxfetchUsersBy(ids: [authorId])
+        .compactMap { $0.first }
+        .do { [weak self] user in
+          self?.steps.accept(AppStep.userProfile(user: user))
+        }
+        .flatMap { _ in Observable<Mutation>.empty() }
     }
   }
 
