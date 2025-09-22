ac9319f560620c45ac17fe6a61cb1813d07f4d80
Luca Park <qlife1146@gmail.com>
2025-09-22 14:42:03 +0900

    🎨: Lint

--- Bragit/Presentation/SignSystem/SignIn/Main/MainLoginViewController.swift
+++ Bragit/Presentation/SignSystem/SignIn/Main/MainLoginViewController.swift
@@ -5,6 +5,8 @@
 //  Created by luca on 8/21/25.
 //
 
+import UIKit
+
 import AuthenticationServices
 import CryptoKit
 import ReactorKit
@@ -12,7 +14,6 @@ import RxCocoa
 import RxSwift
 import SnapKit
 import Then
-import UIKit
 
 final class LoginViewController: UIViewController, View {
   let loginFont = UIFont.pretendard(size: 14, weight: .medium)
